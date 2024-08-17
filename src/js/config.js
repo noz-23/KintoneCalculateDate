f6b4220d3523874c0fa12b0575b1f3ab29a75edd
noz-23 <160399039+noz-23@users.noreply.github.com>
2024-08-17 21:19:52 +0900

    main

--- src/js/config.js
+++ src/js/config.js
@@ -17,95 +17,95 @@
  * History
  *  2024/03/12 0.1.0 初版とりあえずバージョン
  *  2024/03/24 0.2.0 プラグイン設定画面に Google AdSense 追加
- *
+ *  2024/08/17 0.2.1 HTMLの表示変更(とJS変数名変更)
  */
 
 jQuery.noConflict();
 
-(async ( jQuery_,PLUGIN_ID_)=>{
+(async (jQuery_, PLUGIN_ID_) => {
   'use strict';
 
   // 設定パラメータ
-  const ParameterFieldCalculate='paramFieldCalculate';    // 計算フィールド
-  const ParameterFieldDate     ='paramFieldDate';        // 日付フィールド
+  const ParameterFieldCalculate = 'paramFieldCalculate';    // 計算フィールド
+  const ParameterFieldDate = 'paramFieldDate';        // 日付フィールド
 
   // 環境設定
   const Parameter = {
-  // 表示文字
-    Lang:{
-      en:{
-        plugin_titile      : 'Associate Calculated With Date Fields Plugin',
-        plugin_description : 'Sets the result of a calculation in a calculated field to a date field',
-        plugin_label       : 'Please Setting Calculate and Date Field',
-        calc_label         : 'Calculate Field ',
-        date_label         : 'Date Field      ',
-        plugin_cancel      : 'Cancel',
-        plugin_ok          : ' Save ',
-        alert_message      : 'Please don\'t same fields Organizations and Primary'
+    // 表示文字
+    Lang: {
+      en: {
+        plugin_titile: 'Associate Calculated With Date Fields Plugin',
+        plugin_description: 'Sets the result of a calculation in a calculated field to a date field',
+        plugin_label: 'Please Setting Calculate and Date Field',
+
+        label_calc: 'Calculate Field ',
+        label_date: 'Date Field      ',
+
+        plugin_cancel: 'Cancel',
+        plugin_ok: ' Save ',
       },
-      ja:{
-        plugin_titile      : '計算フィールドと日付フィールドの関連付け プラグイン',
-        plugin_description : '計算フィールドで計算した結果を日付フィールドに設定します',
-        plugin_label       : '計算フィールドと日付フィールドを設定して下さい',
-        calc_label         : '計算 フィールド',
-        date_label         : '日付 フィールド',
-        plugin_cancel      : 'キャンセル',
-        plugin_ok          : '   保存  ',
-        alert_message      : '所属組織と優先組織は同じにしないで下さい'
+      ja: {
+        plugin_titile: '計算フィールドと日付フィールドの関連付け プラグイン',
+        plugin_description: '計算フィールドで計算した結果を日付フィールドに設定します',
+        plugin_label: '計算フィールドと日付フィールドを設定して下さい',
+
+        label_calc: '計算 フィールド',
+        label_date: '日付 フィールド',
+
+        plugin_cancel: 'キャンセル',
+        plugin_ok: '   保存  ',
       },
-      DefaultSetting:'ja',
-      UseLang:{}
+      DefaultSetting: 'ja',
+      UseLang: {}
     },
-    Html:{
-      Form               : '#plugin_setting_form',
-      Title              : '#plugin_titile',
-      Description        : '#plugin_description',
-      Label              : '#plugin_label',
-      CalcLabel          : '#calc_label',
-      DateLabel          : '#date_label',
-      Cancel             : '#plugin_cancel',
-      Ok                 : '#plugin_ok',
+    Html: {
+      Form: '#plugin_setting_form',
+      Title: '#plugin_titile',
+      Description: '#plugin_description',
+      Label: '#plugin_label',
+
+      Cancel: '#plugin_cancel',
+      Ok: '#plugin_ok',
     },
-    Elements:{
-      CalcField         :'#calc_field',
-      DateField         :'#date_field',
+    Elements: {
+      CalcField: '#field_calc',
+      DateField: '#field_date',
     },
   };
-  
- 
+
+
   /*
   HTMLタグの削除
    引数　：htmlstr タグ(<>)を含んだ文字列
    戻り値：タグを含まない文字列
   */
-  const escapeHtml =(htmlstr)=>{
+  const escapeHtml = (htmlstr) => {
     return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&quot;').replace(/'/g, '&#39;');
-  };  
+  };
 
   /*
   ユーザーの言語設定の読み込み
    引数　：なし
    戻り値：なし
   */
-  const settingLang=()=>{
+  const settingLang = () => {
     // 言語設定の取得
     Parameter.Lang.UseLang = kintone.getLoginUser().language;
-    switch( Parameter.Lang.UseLang)
-    {
+    switch (Parameter.Lang.UseLang) {
       case 'en':
       case 'ja':
         break;
       default:
-        Parameter.Lang.UseLang =Parameter.Lang.DefaultSetting;
+        Parameter.Lang.UseLang = Parameter.Lang.DefaultSetting;
         break;
     }
     // 言語表示の変更
     var html = jQuery(Parameter.Html.Form).html();
     var tmpl = jQuery.templates(html);
-    
-    var useLanguage =Parameter.Lang[Parameter.Lang.UseLang];
+
+    var useLanguage = Parameter.Lang[Parameter.Lang.UseLang];
     // 置き換え
-    jQuery(Parameter.Html.Form).html(tmpl.render({lang:useLanguage})).show();
+    jQuery(Parameter.Html.Form).html(tmpl.render({ lang: useLanguage })).show();
   };
 
   /*
@@ -113,47 +113,47 @@ jQuery.noConflict();
    引数　：なし
    戻り値：なし
   */
-  const settingHtml= async ()=>{
-    var listFeild =await kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', {'app': kintone.app.getId()});
-    console.log("listFeild:%o",listFeild);
+  const settingHtml = async () => {
+    var listFeild = await kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', { 'app': kintone.app.getId() });
+    console.log("listFeild:%o", listFeild);
 
-    for (const key in listFeild.properties){
+    for (const key in listFeild.properties) {
       //console.log("properties key:%o",key);
       try {
         const prop = listFeild.properties[key];
         //console.log("prop:%o",prop);
-    
+
         // 計算フィールドのみ入れる
-        if (prop.type === 'CALC'){
+        if (prop.type === 'CALC') {
           const option = jQuery('<option/>');
           option.attr('value', escapeHtml(prop.code)).text(escapeHtml(prop.label));
-          console.log("Add CALC option:%o",option);
+          console.log("Add CALC option:%o", option);
           jQuery(Parameter.Elements.CalcField).append(option);
         }
         // 日付フィールドのみ入れる
-        if (prop.type === 'DATE'){
+        if (prop.type === 'DATE') {
           const option = jQuery('<option/>');
           option.attr('value', escapeHtml(prop.code)).text(escapeHtml(prop.label));
 
-          console.log("Add DATE option:%o",option);
+          console.log("Add DATE option:%o", option);
           jQuery(Parameter.Elements.DateField).append(option);
         }
-                 
+
       }
       catch (error) {
-        console.log("error:%o",error);
+        console.log("error:%o", error);
       }
 
       // 現在データの呼び出し
-      var nowConfig =kintone.plugin.app.getConfig(PLUGIN_ID_);
-      console.log("nowConfig:%o",nowConfig);
+      var nowConfig = kintone.plugin.app.getConfig(PLUGIN_ID_);
+      console.log("nowConfig:%o", nowConfig);
 
       // 現在データの表示
-      if(nowConfig[ParameterFieldCalculate]){
-        jQuery(Parameter.Elements.CalcField).val(nowConfig[ParameterFieldCalculate]); 
+      if (nowConfig[ParameterFieldCalculate]) {
+        jQuery(Parameter.Elements.CalcField).val(nowConfig[ParameterFieldCalculate]);
       }
-      if(nowConfig[ParameterFieldDate]){
-        jQuery(Parameter.Elements.DateField).val(nowConfig[ParameterFieldDate]); 
+      if (nowConfig[ParameterFieldDate]) {
+        jQuery(Parameter.Elements.DateField).val(nowConfig[ParameterFieldDate]);
       }
     }
   };
@@ -163,13 +163,13 @@ jQuery.noConflict();
    引数　：なし
    戻り値：なし
   */
-   const saveSetting=()=>{
+  const saveSetting = () => {
     // 各パラメータの保存
-    var config ={};
-    config[ParameterFieldCalculate]=jQuery(Parameter.Elements.CalcField).val();
-    config[ParameterFieldDate]=jQuery(Parameter.Elements.DateField).val();
+    var config = {};
+    config[ParameterFieldCalculate] = jQuery(Parameter.Elements.CalcField).val();
+    config[ParameterFieldDate] = jQuery(Parameter.Elements.DateField).val();
 
-    console.log('config:%o',config);
+    console.log('config:%o', config);
 
     // 設定の保存
     kintone.plugin.app.setConfig(config);
@@ -180,7 +180,7 @@ jQuery.noConflict();
   await settingHtml();
 
   // 保存
-  jQuery(Parameter.Html.Ok).click(() =>{saveSetting();});
+  jQuery(Parameter.Html.Ok).click(() => { saveSetting(); });
   // キャンセル
-  jQuery(Parameter.Html.Cancel).click(()=>{history.back();});
+  jQuery(Parameter.Html.Cancel).click(() => { history.back(); });
 })(jQuery, kintone.$PLUGIN_ID);
